5b16896c0c683e10a1aaae1e351a9c344637df2d
William <william@lexing360.com>
2018-04-04 17:55:36 +0800

    .

--- app/src/main/java/com/uncle/administrator/fleamarket/Sell/SellImageAdapter.java
+++ app/src/main/java/com/uncle/administrator/fleamarket/Sell/SellImageAdapter.java
@@ -3,10 +3,12 @@ package com.uncle.administrator.fleamarket.Sell;
 import android.content.Context;
 import android.databinding.ViewDataBinding;
 
+import com.bumptech.glide.Glide;
 import com.lzy.imagepicker.bean.ImageItem;
 import com.uncle.Base.BaseAdapter;
 import com.uncle.Base.BaseBindViewHolder;
 import com.uncle.administrator.fleamarket.R;
+import com.uncle.administrator.fleamarket.databinding.SellImageItemBinding;
 
 /**
  * @author unclewei
@@ -28,7 +30,8 @@ public class SellImageAdapter extends BaseAdapter<ImageItem> {
         return new BaseBindViewHolder<ImageItem>(binding) {
             @Override
             public void bindTo(BaseBindViewHolder<ImageItem> holder, ImageItem item) {
-
+                SellImageItemBinding sellImageItemBinding = (SellImageItemBinding) binding;
+                Glide.with(getContext()).load(item.path).into(sellImageItemBinding.imageView);
             }
         };
     }
